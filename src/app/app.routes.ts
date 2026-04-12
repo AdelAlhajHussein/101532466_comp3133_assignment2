d7634b4ec6d671db657232a51aabcebddb787f62
AdelAlhajHussein <Adel.AlhajHussein@georgebrown.ca>
2026-04-11 22:51:26 -0400

    adding signup, login and employees

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { Employees } from './pages/employees/employees';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'employees', component: Employees },
+];
